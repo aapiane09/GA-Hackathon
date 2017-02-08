c0858edd2dcdd03bb79449167eb8cc6d9d38e931
shivngiggles <sgogna0912@gmail.com>
2017-02-08 14:21:33 -0800

    Creates route for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(user_params.id)
+    @user = User.find_by_id(params[:id])
   end
 
   def new
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :course, :social_url)
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :social_url, :password, :course)
   end
 
 end
