5441e22aad6468630cc3a2090e147163a85e5f08
shivngiggles <sgogna0912@gmail.com>
2017-02-08 17:36:43 -0800

    Creates event data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find_by_id(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
 end
