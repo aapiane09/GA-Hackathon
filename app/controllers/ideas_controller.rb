0568b4c7fa3b6f4e22e84fc497c661a04a5c6387
c00z <zcusimano@gmail.com>
2017-02-08 20:16:47 -0800

    ideas appearing on event page and on associated user profile

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,13 @@
 class IdeasController < ApplicationController
 
+  # def index
+  #   @event = Event.find_by_id(params[:id])
+  #   @ideas = @event.ideas
+  # end
+
   def index
-    @event = Event.find_by_id(params[:id])
-    @ideas = @event.ideas
+    @user = User.find(params[:user_id])
+    @events = @user.events
   end
 
   def show
@@ -11,14 +16,24 @@ class IdeasController < ApplicationController
 
   def new
     @idea = Idea.new
+    @event = Event.find_by_id(params[:id])
   end
 
   def create
-    # new_idea = Idea.new(idea_params)
-    # @event = Event.find_by_id(params[:event_id])
-    # new_idea.user = current_user
-    # @idea = Idea.create(idea_params)
-    # redirect_to idea_path
+      new_idea = Idea.new(idea_params)
+      @event = Event.find_by_id(params[:id])
+      new_idea.user = current_user
+      new_idea.event = @event
+      @idea = new_idea.save
+      if @idea
+        redirect_to @event
+      else
+        new_idea.errors.full_messages.each do |message|
+          flash[:error] = message
+        end
+         redirect_to new_idea_path
+       end
+
   end
 
   def edit
@@ -27,6 +42,9 @@ class IdeasController < ApplicationController
     #render :edit
   end
 
-  def create
+  private
+  def idea_params
+    params.require(:idea).permit(:title, :content)
   end
+
 end
