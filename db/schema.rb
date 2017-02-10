9aafdecaf7d61eb3d55a1e7449af6b7ca38e37cf
c00z <zcusimano@gmail.com>
2017-02-09 16:03:26 -0800

    added upvoting caching and automatic real time upvoting logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209222230) do
+ActiveRecord::Schema.define(version: 20170209235503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,24 @@ ActiveRecord::Schema.define(version: 20170209222230) do
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "user_id"
     t.integer  "event_id"
+    t.integer  "cached_votes_total",      default: 0
+    t.integer  "cached_votes_score",      default: 0
+    t.integer  "cached_votes_up",         default: 0
+    t.integer  "cached_votes_down",       default: 0
+    t.integer  "cached_weighted_score",   default: 0
+    t.integer  "cached_weighted_total",   default: 0
+    t.float    "cached_weighted_average", default: 0.0
+    t.index ["cached_votes_down"], name: "index_ideas_on_cached_votes_down", using: :btree
+    t.index ["cached_votes_score"], name: "index_ideas_on_cached_votes_score", using: :btree
+    t.index ["cached_votes_total"], name: "index_ideas_on_cached_votes_total", using: :btree
+    t.index ["cached_votes_up"], name: "index_ideas_on_cached_votes_up", using: :btree
+    t.index ["cached_weighted_average"], name: "index_ideas_on_cached_weighted_average", using: :btree
+    t.index ["cached_weighted_score"], name: "index_ideas_on_cached_weighted_score", using: :btree
+    t.index ["cached_weighted_total"], name: "index_ideas_on_cached_weighted_total", using: :btree
     t.index ["event_id"], name: "index_ideas_on_event_id", using: :btree
     t.index ["user_id"], name: "index_ideas_on_user_id", using: :btree
   end
