9ae90b4fcc6822d818cfdf8c538bc04d1613f853
aapiane09 <aapiane09@gmail.com>
2017-02-10 15:55:21 -0800

    Counter updates upon click

--- app/controllers/events_users_controller.rb
+++ app/controllers/events_users_controller.rb
@@ -6,6 +6,7 @@ class EventsUsersController < ApplicationController
     @event = Event.find_by_id(params[:event_id])
     @user = current_user
     @event.users << @user
+    redirect_to :back
   end
 
 end
--- app/models/events_user.rb
+++ app/models/events_user.rb
@@ -1,4 +1,7 @@
 class EventsUser < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  # validates :user_id, uniqueness: true
+
 end
