2594e7728e32be08257f6e66341b94cfeb4cdc66
aapiane09 <aapiane09@gmail.com>
2017-02-12 20:01:41 -0800

    Adds labels to edit forms, fixes .row going over 100% width of the viewport, limits idea index to most recent 3 ideas from first event in db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-    @ideas = Idea.all
+    sorted_ideas = Event.first.ideas.sort_by &:created_at
+    @ideas = sorted_ideas.reverse
     @users = User.all
   end
 
