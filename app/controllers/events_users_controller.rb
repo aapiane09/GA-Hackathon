a56505a58cbf7bd93029433142206ea381a9cf99
aapiane09 <aapiane09@gmail.com>
2017-02-14 09:19:09 -0800

    Fixes adding users to events via event_name

--- app/controllers/events_users_controller.rb
+++ app/controllers/events_users_controller.rb
@@ -3,7 +3,7 @@ class EventsUsersController < ApplicationController
   before_action :require_login, only: [:build]
 
   def build
-    @event = Event.find_by_id(params[:event_id])
+    @event = Event.find_by(params[:event_name])
     @user = current_user
     @event.users << @user
     redirect_to :back
