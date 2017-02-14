4460c3332382ff76999cbf8e7c5e9cce7047f7bc
aapiane09 <aapiane09@gmail.com>
2017-02-14 08:47:23 -0800

    Displays top ideas by most voted

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    sorted_ideas = Event.first.ideas.sort_by &:created_at
+    sorted_ideas = Event.first.ideas.sort_by &:cached_votes_up
     @ideas = sorted_ideas.reverse
     @users = User.all
   end
